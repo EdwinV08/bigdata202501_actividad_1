7333040c504e70ea624ebd0f5269b59ad33507bd
Edwin Villa <edwin.villa@est.iudigital.edu.co>
2025-02-25 15:38:47 -0500

    Carga incial

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bigdata_actividad1",
+    version="0.0.1",
+    author="Edwin Villa",
+    author_email="edwin.villa@est.iudigital.edu.co",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests"
+    ]
+    
+    
+)
